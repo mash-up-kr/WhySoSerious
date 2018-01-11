5fa5ab6f69c6de99be5aea1a3648c0389ee17702
윤민섭 <yoonminseob2@naver.com>
2018-01-11 22:46:52 +0900

    파일 생성

---
+++ WhySoSerious/Source/Public/Opinion.swift
@@ -0,0 +1,15 @@
+//
+//  Opinion.swift
+//  WhySoSerious
+//
+//  Created by 윤민섭 on 2018. 1. 9..
+//  Copyright © 2018년 MashUp. All rights reserved.
+//
+
+import RxCocoa
+import RxSwift
+
+class OpinionViewController: UIViewController {
+    
+    
+}
